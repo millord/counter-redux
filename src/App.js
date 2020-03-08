e357f9a1ac146e33a39d5f495ecf7a7bcb6bb22c
millord <juancarlosmillord@gmail.com>
2020-03-08 16:24:23 -0400

    just redux counter app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-function App() {
+import { increment, decrement } from "./actions";
+
+const App = () => {
+  const { counter, isLogged } = useSelector(state => state);
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to Redux! :)</h1>
+      <p>{isLogged ? "The user is logged in" : "The user is not log in"}</p>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <p>this is the counter: {counter}</p>
     </div>
   );
-}
+};
 
 export default App;
